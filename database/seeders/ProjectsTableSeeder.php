73c607e031f7f251b03ef5afbfcb7d009d3ef3b7
mirkobechini <mirkobechini@gmail.com>
2026-01-22 15:03:39 +0100

    Add: create Project model Project.php create Project creating table migration create project seeder ProjectsTableSeeder.php

---
+++ database/seeders/ProjectsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class ProjectsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for($i=0; $i<10; $i++){
+            $newProject = new Project();
+            $newProject->title = $faker->sentence(3);
+            $newProject->customer = $faker->company();
+            $newProject->description = $faker->paragraphs(3, true);
+            $newProject->cover_image = $faker->imageUrl(640, 480, 'projects', true);
+            $newProject->save();
+        }
+    }
+}
